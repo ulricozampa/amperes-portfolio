d3473a5269c174e74bc1a7c726fa2653362115aa
Ulrico Zampa <ulricozampa@gmail.com>
2026-01-06 20:39:35 -0500

    Add prestigious exhibitions with creative awards

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   metadataBase: new URL('https://amperes.work'),
   title: "Ulrico Umberto Zampa",
-  description: "Full-time reckless opportunist",
+  description: "--",
   openGraph: {
     title: "Ulrico Umberto Zampa",
-    description: "Full-time reckless opportunist",
+    description: "--",
     images: ["/klogo.jpg"],
   },
   twitter: {
     card: "summary_large_image",
     title: "Ulrico Umberto Zampa",
-    description: "Full-time reckless opportunist",
+    description: "--",
     images: ["/klogo.jpg"],
   },
 };
